ed97364a407af376ce872a7385dad13c418a5177
NickolaNikolov <71521171+NickolaNikolov@users.noreply.github.com>
2021-06-21 18:04:51 +0300

    Toggle Btn and Media Queries

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import {Switch, Route} from 'react-router-dom';
 import AboutPage from './Pages/AboutPage';
 import SolutionPage from './Pages/SolutionPage';
 import ContactPage from './Pages/ContactPage';
+import {useState} from 'react';
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () => {
+      setNavToggle(!navToggle)
+  }
   return (
     <div className="App">
-        <div className="sidebar">
+        <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
             <NavBar />
+        </div>
+        <div class="nav-btn" onClick={navClick}>
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="line-3"></div>
         </div>
         <div className="main-content">
             <div className="content">
