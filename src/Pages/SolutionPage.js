1b0ca04600dbbc0e8d78548330480e2ba27008a5
NickolaNikolov <71521171+NickolaNikolov@users.noreply.github.com>
2021-06-28 17:58:32 +0300

    Small changes

--- src/Pages/SolutionPage.js
+++ src/Pages/SolutionPage.js
@@ -8,7 +8,7 @@ const SolutionPage = () => {
             <table className="table" border="1" id="my-table">
                 <thead>
                     <tr>
-                        <th>Предпр./ Б. планове</th>
+                        <th>Предпр./ Б.планове</th>
                         <th>Предприятие 1</th>
                         <th>Предприятие 2</th>
                         <th>Предприятие 3</th>
@@ -84,9 +84,41 @@ const SolutionPage = () => {
                             <input type="num" id="number" name="income33" placeholder="Печалба 3.3"/>
                         </td>
                     </tr>
+                    <tr>
+                        <th rowspan="2">Бизнес план 4</th>
+                        <td>
+                            <input type="num" id="number" name="invest41" placeholder="Инвестиция 4.1"/>
+                        </td>
+                        <td>
+                            <input type="num" id="number" name="invest42" placeholder="Инвестиция 4.2"/>
+                        </td>
+                        <td>
+                            <input type="num" id="number" name="invest43" placeholder="Инвестиция 4.3"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="num" id="number" name="income41" placeholder="Печалба 4.1"/>
+                        </td>
+                        <td>
+                            <input type="num" id="number" name="income42" placeholder="Печалба 4.2"/>
+                        </td>
+                        <td>
+                            <input type="num" id="number" name="income43" placeholder="Печалба 4.3"/>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
             <button className="table-button">Изпрати данни</button>
+            <p className="result-info">
+                Максималната печалба е <span><strong>9</strong></span> млн. евро, 
+                при условие че <span><strong>първото</strong></span> предприятие 
+                използва <span><strong>първия</strong></span> бизнес
+                план, <span><strong>второто</strong></span> предприятие - 
+                <span><strong> първия</strong></span> бизнес план и 
+                <span><strong> третото</strong></span> предприятие - 
+                <span><strong> втория</strong></span> бизнес план.
+            </p>
         </div>
     );
 }
